8844c395cafa2fa64a3606198cb64d81e1d44412
look638 <106069362+look638@users.noreply.github.com>
2022-07-22 20:18:39 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+// 1 Переменные
+alert(1)
+
+const firstName = "Vladlen"
+//const age = "17"
+
+const widthNum5 ="5"
+const IsProgammer = "true"
+
+// 2 Мультирование
+
+//console.log(" Имя человека: " + firstName + "  возраст:" + age)
+//console.log(age) // age.toSring() - строка , age- число
+
+ //const lastName = prompt("Введите фамилию")
+ //alert(firstName + " " + lastName )
+
+ //3 Операторы
+
+ let currentYear = 2022 //const-нельзя изменить 
+ let birthYear = 2004 //let-можно изменить
+
+ //const age = currentYear - birthYear
+
+ //console.log(age)
+
+ const a = 10
+ const b = 5
+
+ console.log(a + b)
+ console.log(a - b)
+ console.log(a / b)
+ console.log(a * b)
+ console.log(a ** b)
+ 
+ console.log(birthYear++ )
+ console.log(birthYear++ )
+ console.log(birthYear)
+ 
+ console.log(currentYear++ )
+ console.log(currentYear-- )
+ console.log(currentYear)
