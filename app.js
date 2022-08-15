c943a486ae5d654b84f5ee52e49aac90a225b439
look638 <106069362+look638@users.noreply.github.com>
2022-08-15 16:35:02 +0700

    html cs js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // 1 Переменные
 alert(1)
 
-const firstName = "Vladlen"
+//const firstName = "Vladlen"
 //const age = "17"
 
-const widthNum5 ="5"
-const IsProgammer = "true"
+//const widthNum5 ="5"
+//const IsProgammer = "true"
 
-// 2 Мультирование
+// 2 Мутирование
 
 //console.log(" Имя человека: " + firstName + "  возраст:" + age)
 //console.log(age) // age.toSring() - строка , age- число
@@ -17,26 +17,33 @@ const IsProgammer = "true"
 
  //3 Операторы
 
- let currentYear = 2022 //const-нельзя изменить 
- let birthYear = 2004 //let-можно изменить
+ //let currentYear = 2022 //const-нельзя изменить 
+ //let birthYear = 2004 //let-можно изменить
 
  //const age = currentYear - birthYear
 
  //console.log(age)
 
- const a = 10
- const b = 5
+// const a = 10
+ //const b = 5
 
- console.log(a + b)
- console.log(a - b)
- console.log(a / b)
- console.log(a * b)
- console.log(a ** b)
+ //console.log(a + b)
+ //console.log(a - b)
+ //.log(a / b)
+ //console.log(a * b)
+ //console.log(a ** b)
  
- console.log(birthYear++ )
- console.log(birthYear++ )
- console.log(birthYear)
+ //console.log(birthYear++ )
+ //console.log(birthYear++ )
+ //console.log(birthYear)
  
- console.log(currentYear++ )
- console.log(currentYear-- )
- console.log(currentYear)
+ //console.log(currentYear++ )
+ //console.log(currentYear-- )
+ //console.log(currentYear)
+
+ // 4 Типы данных
+
+ const isProgrammer = true
+ const name = "Vlad"
+ const age = 26
+ console.log(typeof isProgrammer)
---
+++ угадай число.js
@@ -0,0 +1,129 @@
+/*var randomNumber = Math.floor(Math.random() * 100) + 1;
+
+var guesses = document.querySelector("guesses");
+var lastResult = document.querySelector("lastResult");
+var lowOrHi = document.querySelector("lowOrHi");
+
+var guessSubmit = document.querySelector(".guessSubmit");
+var guessFild = document.querySelector(".guessFild");
+
+var guessCount = 1;
+var resetButton;
+
+function checkGuess() {
+    var userGuess = Number(guessFild.value);
+    if (guessCount ===1) {
+        guesses.textContent = "Введите число: ";
+    }
+
+    guesses.textContent += userGuess + "";
+
+    if (userGuess === randomNumber) {
+        lastResult.textContent = "Угадал";
+        lastResult.style.backgroundColor = "green";
+        lowOrHi.textContent = "";
+
+        setGameOver();
+    } else if (guessCount === 10) {
+        lastResult.textContent = "Gay Over"
+
+        setGameOver();
+
+    } else {
+        lastResult.textContent = "lox";
+        lastResult.style.backgroundColor = "red";
+        if(userGuess < randomNumber) {
+            lowOrHi.textContent = "Маловато";
+
+    } else if (userGuess > randomNumber) {
+        lowOrHi.textContent = "Многовато";
+    }
+    
+}
+
+guessCount++;
+guessFild.value = "";
+guessFild.focus();
+
+}
+
+guessSubmit.addEventListener('click', checkGuess);*/
+
+
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+const guesses = document.querySelector(".guesses");
+const lastResult = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
+const guessSubmit = document.querySelector(".guessSubmit");
+const guessField = document.querySelector(".guessField");
+let guessCount = 1;
+let resetButton;
+
+function checkGuess() {
+  const userGuess = Number(guessField.value);
+  if (guessCount === 1) {
+    guesses.textContent = "Previous guesses: ";
+  }
+
+  guesses.textContent += userGuess + " ";
+
+  if (userGuess === randomNumber) {
+    lastResult.textContent = "Congratulations! You got it right!";
+    lastResult.style.backgroundColor = "green";
+    lowOrHi.textContent = " ";
+    setGameOver();
+  } else if (guessCount === 10) {
+    lastResult.textContent = "Gay Over";
+    lowOrHi.textContent = " ";
+    setGameOver();
+  } else {
+    lastResult.textContent = "Вадя лох";
+    lastResult.style.backgroundColor = "red";
+    if(userGuess < randomNumber) {
+      lowOrHi.textContent = "Маловато" ;
+    } else if(userGuess > randomNumber) {
+      lowOrHi.textContent = "Многовато";
+    }
+  }
+
+  guessCount++;
+  guessField.value = " ";
+  guessField.focus();
+}
+
+guessSubmit.addEventListener("click", checkGuess);
+
+function setGameOver() {
+  guessField.disabled = true;
+  guessSubmit.disabled = true;
+  resetButton = document.createElement("button");
+  resetButton.textContent = "Начать новую игру";
+  document.body.appendChild(resetButton);
+  resetButton.addEventListener("click", resetGame);
+
+}
+
+function resetGame () {
+  guessCount = 1;
+  
+  var resetPaaras = document.querySelectorAll(".resultParas p");
+  for (var i = 0 ; i<resetPaaras.length ; i++) {
+    resetPaaras[i].textContent = "";
+  }
+
+  resetButton.parentNode.removeChild(resetButton);
+
+  guessField.disabled = false;
+  guessSubmit.disabled = false;
+  guessField.value = "";
+  guessField.focus();
+
+  lastResult.style.backgroundColor = "white";
+
+  let randomNumber = Math.floor(Math.random() * 100) + 1;
+}
+
+
+
+
+
